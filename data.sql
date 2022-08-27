e25868c596e2ad0accae5deb35965aa2d170c5ed
shaheer ch <shaheer.chry@gmail.com>
2022-08-27 15:05:44 +0500

    Create Database and multiple sql files

---
+++ data.sql
@@ -0,0 +1,9 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals VALUES(1,'Agumon','Feb 03, 2020',0,true,10.23);
+
+INSERT INTO animals VALUES(2,'Gabumon','Nov 15, 2018',2,true,8);
+
+INSERT INTO animals VALUES(3,'Pikachu','Jan 07, 2021',1,false,15.04);
+
+INSERT INTO animals VALUES(4,'Devimon','May 12, 2017',5,true,11);
