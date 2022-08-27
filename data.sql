11b86d58f3550965ff90024b9dcdb394055473c1
shaheer ch <shaheer.chry@gmail.com>
2022-08-27 20:44:50 +0500

    Update-and-query

--- data.sql
+++ data.sql
@@ -7,3 +7,17 @@ INSERT INTO animals VALUES(2,'Gabumon','Nov 15, 2018',2,true,8);
 INSERT INTO animals VALUES(3,'Pikachu','Jan 07, 2021',1,false,15.04);
 
 INSERT INTO animals VALUES(4,'Devimon','May 12, 2017',5,true,11);
+
+ INSERT INTO animals VALUES(5,'Charmander', 'feb 08,2020',0,false,-11);
+
+ INSERT INTO animals VALUES(6,'Plantmon', 'nov 15,2021',2,true,-5.7);
+
+ INSERT INTO animals VALUES(7,'Squirtle', 'apr 02,1993',3,false,-12.13);
+
+ INSERT INTO animals VALUES(8,'Angemon', 'jul 07,2005',1,true,-45);
+
+ INSERT INTO animals VALUES(9,'Boarmon', 'jul 07,2005',1,true,20.14);
+
+ INSERT INTO animals VALUES(10,'Blossom', 'oct 13,1998',3,true,17);
+
+ INSERT INTO animals VALUES(11,'Ditto', 'may 14,2022',4,true,22,);
