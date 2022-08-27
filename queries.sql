884413effa21803bdb6d94aad8a98379f63d200e
shaheer ch <shaheer.chry@gmail.com>
2022-08-28 01:52:30 +0500

    Visits

--- queries.sql
+++ queries.sql
@@ -29,16 +29,16 @@ ROLLBACK
  
 
  
-SELECT NAME FROM ANIMALS WHERE NEUTERED = true AND ESCAPE_ATTEMPTS < 3;
+SELECT NAME FROM animals WHERE NEUTERED = true AND ESCAPE_ATTEMPTS < 3;
 
-SELECT NAME, DATE_OF_BIRTH FROM ANIMALS WHERE NAME IN ('Agumon','Pikachu');
+SELECT NAME, DATE_OF_BIRTH FROM animals WHERE NAME IN ('Agumon','Pikachu');
 
-SELECT NAME, ESCAPE_ATTEMPTS FROM ANIMALS WHERE WEIGHT_KG > 10.5;
+SELECT NAME, ESCAPE_ATTEMPTS FROM animals WHERE WEIGHT_KG > 10.5;
 
  
-SELECT * FROM ANIMALS WHERE NEUTERED = TRUE;
+SELECT * FROM animals WHERE NEUTERED = TRUE;
 
-SELECT * FROM ANIMALS WHERE NOT NAME = 'Gabumon';
+SELECT * FROM animals WHERE NOT NAME = 'Gabumon';
 
 select count(*) from animals;
 
@@ -92,14 +92,14 @@ ALTER TABLE
 alter table animals add constraint FK_owners FOREIGN KEY (owners_id) REFERENCES owners(id);
 ALTER TABLE
 
-UPDATE ANIMALS 
+UPDATE animals 
 set species='unspecified';
 
-update ANIMALS
+update animals
 set species = 'digimon'
 where name like '%mon';
 
-update ANIMALS
+update animals
 set species = 'pokemon'
 where not species = 'digimon';
 
@@ -113,7 +113,7 @@ SELECT species.name AS species FROM species JOIN animals ON species.id = species
 
 SELECT owners.full_name AS owners, animals.name AS animals FROM owners INNER JOIN species ON owners.full_name= 'Jenifer Orwell' AND species.name =' Digimon'INNER  JOIN animals ON owners.id= owners_id AND species.id= species_id;
 
-SELECT owners.full_name as Owners,animals.name as Animals FROM owners JOIN  animals ON   Owners.id = owner_id AND owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+SELECT owners.full_name as Owners,animals.name as animals FROM owners JOIN  animals ON   Owners.id = owner_id AND owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
 
 SELECT owners.full_name AS Owners,count(*) FROM owners JOIN  animals ON  owners.id = owner_id GROUP BY Owners ORDER BY count DESC LIMIT 1; 
 
@@ -130,3 +130,27 @@ UPDATE animals
 UPDATE animals
    SET owner_id = (SELECT id from owners WHERE full_name = 'Sam Smith')
    WHERE name = 'Agumon';
+
+
+SELECT animals.name FROM animals JOIN  visits ON  animals.id = animals_id JOIN vets ON vets_id = vets.id WHERE vets.name = 'William Tatcher' ORDER BY date_of_visit DESC LIMIT 1;
+
+ 
+SELECT COUNT(DISTINCT animals.name) FROM animals JOIN  visits ON  animals.id = animals_id JOIN vets ON vets_id = vets.id WHERE vets.name = 'Stephanie Mendez';
+
+ 
+SELECT vets.name AS vet,species.name AS specialization FROM vets JOIN  specialization ON  vets.id = vets_id JOIN species ON species_id = species.id;
+
+ 
+SELECT animals.name FROM animals JOIN  visits ON  animals.id = animals_id JOIN vets ON vets_id = vets.id WHERE vets.name = 'Stephanie Mendez' AND date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
+
+SELECT animals.name, count(*) FROM animals JOIN  visits ON  animals.id = animals_id JOIN vets ON vets_id = vets.id GROUP BY animals.name ORDER BY count DESC LIMIT 1;
+
+ 
+SELECT animals.name,date_of_visit FROM animals JOIN  visits ON  animals.id = animals_id JOIN vets ON vets_id = vets.id WHERE vets.name = 'Maisy Smith' ORDER BY date_of_visit LIMIT 1;
+
+ 
+SELECT animals.name,date_of_birth,escape_attempts,neutered,weight_kg,species.name as specie, vets.name as vet_name,vets.age as vet_age,date_of_graduation ,date_of_visit FROM species JOIN animals ON animals.species_id = species.id JOIN  visits ON  animals.id = animals_id JOIN vets ON visits.vets_id = vets.id ORDER BY date_of_visit DESC LIMIT 1;
+
+SELECT vets.name,count(*) FROM visits JOIN  vets ON vets.id = visits.vets_id LEFT JOIN specialization ON vets.id = specialization.vets_id WHERE specialization.vets_id IS NULL GROUP BY vets.name;
+
+SELECT species.name AS specialization,count(*) AS number_of_visits FROM species JOIN  animals ON species.id = animals.species_id JOIN visits ON animals.id  = visits.animals_id JOIN vets ON visits.vets_id = vets.id  WHERE vets.name = 'Maisy Smith' GROUP BY species.name ORDER BY number_of_visits DESC LIMIT 1;
